3b916d96212b3b195a2dce3bff644dc8e2a71731
Michael Cuison <77145878+mtcuison@users.noreply.github.com>
2025-09-09 13:37:29 +0800

    Initial commit

---
+++ src/main/java/ph/com/guanzongroup/cas/purchasing/t2/CasPurchasingT2.java
@@ -0,0 +1,8 @@
+package ph.com.guanzongroup.cas.purchasing.t2;
+
+public class CasPurchasingT2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
